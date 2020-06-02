020cb529dae66d40c28a71761100e0a93e83cb8d
Rogerio Azevedo <raazeved@hotmail.com>
2020-06-01 22:06:10 -0300

    ajustes

--- App/Cloud/POC/aplicacao/s3-conf.php
+++
@@ -1,7 +0,0 @@
-<?php
-$accessKey = 'AKIA4UPIHQLWBXOKR3J3';
-$secretKey = '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';
-$region = 'us-east-1';
-$bucket = 'imagens-bootcamp-cloud1';
-$arqName =  'logo.jpg';
-?>
